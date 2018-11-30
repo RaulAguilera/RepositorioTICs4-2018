bbc1dd4aa4c16df9210c55aca067ba273133892a
raul.aguilera <jrat9fra@gmail.com>
2018-11-30 08:05:17 -0600

    Added example to read xml and display content of nodes

--- EjemplosXml/Program.cs
+++ EjemplosXml/Program.cs
@@ -8,10 +8,35 @@ namespace EjemplosXml
     {
         static void Main(string[] args)
         {
+            EjemploLeerArchivoYMostrarContenido();
+        }
 
+
+        public static void EjemploLeerArchivoYMostrarContenido()
+        {
+            XmlDocument xmlDoc = new XmlDocument(); //Creamos un documento XML en memoria
+            xmlDoc.Load(File.Open("notasNov30.xml",FileMode.Open)); //Cargamos el contenido de notasNov30.xml
+
+            XmlNodeList nodeList = xmlDoc.DocumentElement.ChildNodes;//Obtenemos todos los nodos hijo
+                                                                     //incluidos en el nodo padre de notasNov30.xml
+                                                                     //y los guardamos en la variable nodeList
+            foreach(XmlNode node in nodeList)//Iteramos a los largo de la lista de nodos nodeList para obtener
+            {                                // su texto interno
+                Console.WriteLine(node.InnerText); //Imprimimos el contenido del nodo
+            }
+
+            
+            Console.ReadKey();
+        }
+
+        public static void EjemploCrearArchivo()
+        {
             //CREAR DOCUMENTO EN MEMORIA
             XmlDocument xmlDoc = new XmlDocument(); //Creamos un documento XML en memoria
             xmlDoc.LoadXml("<notasPadre></notasPadre>");//Le asignamos nuevo nodo Padre a nuestro nuevo documento
+
+            
+
 
             //MODIFICAR DOCUMENTO EN MEMORIA
             //Agregar un nodo hijo a el nodo padre del documento
@@ -31,6 +56,7 @@ namespace EjemplosXml
 
             //Guardamos el documento físicamente
             xmlDoc.Save(writer);
+
         }
     }
 }
